bced04f37a1f775156b7d4d2754c3ed0a49f4506
Kaique <kaique1245br@gmail.com>
2025-02-18 16:00:22 -0300

    Alterações em login

--- php/login.php
+++ php/login.php
@@ -11,9 +11,21 @@
 </head>
 
 <body>
-    <header></header>
-    <main></main>
-    <footer></footer>
+    <main>
+        <video id="background_video" src="../videos/videoplayback.mp4" autoplay loop muted></video>
+        <div id="container">
+            <form action="POST" method="POST" id="form_login">
+                <h1>Entrar</h1>
+                <input type="email" id="email_login" placeholder="E-mail">
+
+                <input type="password" id="senha_login" placeholder="Senha">
+
+                <a href="" id="esqueci_senha">Esqueci senha</a>
+
+                <button type="submit" id="entrar_login">Entrar</button>
+            </form>
+        </div>
+    </main>
 </body>
 
 </html>
