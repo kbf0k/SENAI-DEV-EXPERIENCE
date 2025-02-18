f2106aa1be4d9bef5683eb04c8777b3bb6061565
Kaique <kaique1245br@gmail.com>
2025-02-18 13:35:54 -0300

    First commit

---
+++ php/apoiadores.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="Pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/apoiadores.css">
+    <script src="../js/apoiadores.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
+    <title>Apoaidores</title>
+</head>
+
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
---
+++ php/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="Pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/login.css">
+    <script src="../js/login.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
+    <title>Login</title>
+</head>
+
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
